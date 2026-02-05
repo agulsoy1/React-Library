4ce9c3e9f1d05a591ba604b694fb375a7d52dd68
agulsoy1 <alexandretg99@gmail.com>
2026-02-04 19:35:50 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Nav from "./Components/Nav";
+import Footer from "./Components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Books from "./Pages/Books";
+import { books } from "./data";
+import BookInfo from "./Pages/BookInfo";
+// import Book from "./Components/UI/Book";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" render={() => <Books books={books} />} />
+        <Route path="/books/1" render={() => <BookInfo books={books} />} />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
